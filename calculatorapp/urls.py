2c60fe21c3a6d10e7f8ae202b91778c1de1b6aa4
hahwyd <hahwyd@gmail.com>
2024-11-19 16:54:12 +0100

    First commit

---
+++ calculatorapp/urls.py
@@ -0,0 +1,9 @@
+# calculatopapp urls
+from django.urls import path
+from .views import home, result
+
+
+urlpatterns = [
+    path("", home, name="home"),
+    path("result/", result, name="resullt"),
+]
